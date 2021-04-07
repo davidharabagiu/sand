6d679a12f55949160dd2f38ddbb6c35c874aafd5
David Harabagiu <harabagiudavid@gmail.com>
2021-04-07 21:13:25 +0300

    Integrate openssl

--- sand/libsand/hello/hello.cpp
+++ sand/libsand/hello/hello.cpp
@@ -1,8 +1,13 @@
 #include "sand/hello.hpp"
 
+#include <iomanip>
 #include <iostream>
+#include <sstream>
+
+#include <cstring>
 
 #include <glog/logging.h>
+#include <openssl/sha.h>
 
 SAND_API_CTOR void init()
 {
@@ -16,4 +21,15 @@ SAND_API_DTOR void uninit()
 void sand::LogSomething()
 {
     LOG(INFO) << "Hello";
+
+    const char *  data = "test";
+    unsigned char hash[SHA512_DIGEST_LENGTH];
+    SHA512(reinterpret_cast<const unsigned char *>(data), std::strlen(data), hash);
+
+    std::ostringstream ss;
+    for (auto byte : hash)
+    {
+        ss << std::hex << std::setw(2) << std::setfill('0') << unsigned {byte};
+    }
+    LOG(INFO) << ss.str();
 }
