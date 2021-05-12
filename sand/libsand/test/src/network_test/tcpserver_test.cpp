4528ac56604d4bc935119b1200f0905a69d4123a
David Harabagiu <harabagiudavid@gmail.com>
2021-05-13 01:41:07 +0300

    Unit test: ReceiveMessage_MultipleConnectionsInParallel

--- sand/libsand/test/src/network_test/tcpserver_test.cpp
+++ sand/libsand/test/src/network_test/tcpserver_test.cpp
@@ -29,13 +29,14 @@ protected:
     }
 
     io_context                              server_context_;
-    io_context                              sender_context_;
+    io_context                              sender1_context_;
+    io_context                              sender2_context_;
     std::shared_ptr<TCPMessageListenerMock> listener_mock_;
     const unsigned short                    port_ = 1234;
 };
 }  // namespace
 
-TEST_F(TCPServerTest, ReceiveMessage)
+TEST_F(TCPServerTest, ReceiveMessage_SingleConnection)
 {
     TCPServerImpl server {server_context_, port_};
     server.register_listener(listener_mock_);
@@ -58,7 +59,7 @@ TEST_F(TCPServerTest, ReceiveMessage)
             cv.notify_one();
         });
 
-    tcp::socket socket {sender_context_};
+    tcp::socket socket {sender1_context_};
     socket.connect({address::from_string("127.0.0.1"), port_});
     error_code err;
     write(socket, buffer(msg), err);
@@ -72,5 +73,72 @@ TEST_F(TCPServerTest, ReceiveMessage)
     EXPECT_TRUE(msg_received);
 
     server_context_.stop();
+    server_thread.join();
+}
+
+TEST_F(TCPServerTest, ReceiveMessage_MultipleConnectionsInParallel)
+{
+    TCPServerImpl server {server_context_, port_};
+    server.register_listener(listener_mock_);
+    std::thread server_thread {[&] { server_context_.run(); }};
+
+    const std::string msg1 {"Hai cu berea, hai cu vinu"};
+    const std::string msg2 {"Tigan infractor"};
+
+    bool                    msg1_received = false;
+    bool                    msg2_received = false;
+    std::mutex              mut;
+    std::condition_variable cv;
+
+    EXPECT_CALL(
+        *listener_mock_, on_message_received(conversion::to_ipv4_address("127.0.0.1"), _, _))
+        .With(Args<1, 2>(ElementsAreArray(msg1.begin(), msg1.end())))
+        .Times(1)
+        .WillOnce([&](...) {
+            {
+                std::lock_guard lock(mut);
+                msg1_received = true;
+            }
+            cv.notify_one();
+        });
+    EXPECT_CALL(
+        *listener_mock_, on_message_received(conversion::to_ipv4_address("127.0.0.1"), _, _))
+        .With(Args<1, 2>(ElementsAreArray(msg2.begin(), msg2.end())))
+        .Times(1)
+        .WillOnce([&](...) {
+            {
+                std::lock_guard lock(mut);
+                msg2_received = true;
+            }
+            cv.notify_one();
+        });
+
+    std::thread sender1_thread {[&] {
+        tcp::socket socket {sender1_context_};
+        socket.connect({address::from_string("127.0.0.1"), port_});
+        error_code err;
+        write(socket, buffer(msg1), err);
+        EXPECT_FALSE(err);
+        socket.close();
+    }};
+    std::thread sender2_thread {[&] {
+        tcp::socket socket {sender2_context_};
+        socket.connect({address::from_string("127.0.0.1"), port_});
+        error_code err;
+        write(socket, buffer(msg2), err);
+        EXPECT_FALSE(err);
+        socket.close();
+    }};
+
+    {
+        std::unique_lock lock {mut};
+        cv.wait_for(
+            lock, std::chrono::milliseconds {100}, [&] { return msg1_received && msg2_received; });
+    }
+    EXPECT_TRUE(msg2_received);
+
+    server_context_.stop();
+    sender1_thread.join();
+    sender2_thread.join();
     server_thread.join();
 }
