da9531d6e29e7bbd2c84b359fbbfbb890e82b9bc
David Harabagiu <harabagiudavid@gmail.com>
2021-06-18 18:47:24 +0300

    Mocked AESCipher

---
+++ sand/libsand/test/inc/private/aescipher_mock.hpp
@@ -0,0 +1,27 @@
+#ifndef SAND_TEST_AESCIPHER_MOCK_HPP_
+#define SAND_TEST_AESCIPHER_MOCK_HPP_
+
+#include <gmock/gmock.h>
+
+#include "aescipher.hpp"
+
+using namespace ::sand::crypto;
+using namespace ::sand::utils;
+
+namespace sand
+{
+class AESCipherMock : AESCipher
+{
+public:
+    MOCK_METHOD(bool, generate_key_and_iv, (KeySize, ModeOfOperation, ByteVector &, ByteVector &),
+        (const, override));
+    MOCK_METHOD(std::future<ByteVector>, encrypt,
+        (ModeOfOperation, const ByteVector &, const ByteVector &, const ByteVector &, Executer &),
+        (override));
+    MOCK_METHOD(std::future<ByteVector>, decrypt,
+        (ModeOfOperation, const ByteVector &, const ByteVector &, const ByteVector &, Executer &),
+        (override));
+};
+}  // namespace sand
+
+#endif  // SAND_TEST_AESCIPHER_MOCK_HPP_
